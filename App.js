52260027ac765cb83aa78f8189d9dc20c3f972f2
lmejiaf <lmejiaf@uninorte.edu.co>
2020-10-25 13:14:50 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Pressable,
+  TextInput,
+} from 'react-native';
+import { tailwind, getColor } from './src/tailwind';
+
+
+
+
+const App = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+
+export default App;
